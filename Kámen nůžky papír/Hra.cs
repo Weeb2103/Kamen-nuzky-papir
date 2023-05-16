34986e474ce900a1f9d055a7007d27beabdab128
Jiri.Vondracek <jiri.vondracek@educhem.cz>
2023-05-16 12:16:45 +0200

    Přidejte soubory projektu.

---
+++ Kámen nůžky papír/Hra.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kámen_nůžky_papír
+{
+    class Hra
+    {
+        public int SkoreHrac { get; set; }
+        public int SkoreNepritel { get; set; }
+        public Random generatorVoleb = new Random();
+        public Hra()
+        {
+            SkoreHrac = 0;
+            SkoreNepritel = 0;
+        }
+        public void ZvyseniSkore(bool vyhraHrace)
+        {
+            if (vyhraHrace)
+            {
+                SkoreHrac++;
+            }
+            else
+            {
+                SkoreNepritel++;
+            }
+        }
+        public void Remiza()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Remíza / Tie");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        public void Vyhra()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hráč vyhrál / Player won");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        public void Prohra()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Nepřítel vyhrál / Opponent won");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+}
---
+++ Kámen nůžky papír/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Threading;
+using Kámen_nůžky_papír;
+
+Hra hra = new Hra();
+Console.WriteLine("Vítej ve hře kámen - nůžky - papír / Welcome at rock - paper - scissors");
+Console.WriteLine("Hra začíná za... / Game starts in...");
+Console.WriteLine("3");
+Thread.Sleep(1000);
+Console.WriteLine("2");
+Thread.Sleep(1000);
+Console.WriteLine("1");
+Thread.Sleep(1000);
+Console.Clear();
+while (hra.SkoreHrac < 5 && hra.SkoreNepritel < 5)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hráč / Player: " + hra.SkoreHrac);
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Nepřítel / Opponent: " + hra.SkoreNepritel);
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("-------------");
+    Console.WriteLine("1 - kámen / rock");
+    Console.WriteLine("2 - nůžky / scissors");
+    Console.WriteLine("3 - papír / paper");
+    Console.WriteLine("-------------");
+    ConsoleKeyInfo vyberHrace = Console.ReadKey(true);
+    Random generatorVoleb = new Random();
+    int vyberNepritele = generatorVoleb.Next(3);
+    switch (vyberHrace.Key)
+    {
+        case ConsoleKey.D1:
+        case ConsoleKey.NumPad1:
+            if (vyberNepritele == 1)
+            {
+                hra.Remiza();
+                break;
+            }
+            else if (vyberNepritele == 2)
+            {
+                hra.Vyhra();
+                hra.ZvyseniSkore(true);
+                break;
+            }
+            else
+            {
+                hra.Prohra();
+                hra.ZvyseniSkore(false);
+                break;
+            }
+        case ConsoleKey.D2:
+        case ConsoleKey.NumPad2:
+            if (vyberNepritele == 1)
+            {
+                hra.Prohra();
+                hra.ZvyseniSkore(false);
+                break;
+            }
+            else if (vyberNepritele == 2)
+            {
+                hra.Remiza();
+                break;
+            }
+            else
+            {
+                hra.Vyhra();
+                hra.ZvyseniSkore(true);
+                break;
+            }
+        case ConsoleKey.D3:
+        case ConsoleKey.NumPad3:
+            if (vyberNepritele == 1)
+            {
+                hra.Vyhra();
+                hra.ZvyseniSkore(true);
+                break;
+            }
+            else if (vyberNepritele == 2)
+            {
+                hra.Prohra();
+                hra.ZvyseniSkore(false);
+                break;
+            }
+            else
+            {
+                hra.Remiza();
+                break;
+            }
+        default:
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Špatně zadaná hodnota - zadej vstup znovu / Incorrectly entered value - enter the input again ");
+            Thread.Sleep(2000);
+            Console.Clear();
+            break;
+    }
+}
+if (hra.SkoreHrac > hra.SkoreNepritel)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hráč vyhrál / Player won");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Nepřítel vyhrál / Opponent won");
+    Console.ForegroundColor = ConsoleColor.White;
+}
